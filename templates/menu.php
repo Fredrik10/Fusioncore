c4242c6cd877b43a6cf741856bbdeea9f02cd96d
elev <elev@5CG62366CS>
2018-11-13 18:14:08 +0100

    php finns nu
    Fler filer bättre betyg

---
+++ templates/menu.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+
+
+<nav>
+<div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="index.html">Homepage &#9916;</a>
+                <a href="artiklar.html">Patch Notes &#9916;</a>
+                <a href="fusioncore.html">Fusion Core &#9916;</a>
+                <a href="kontakter.html">Contact &#9916;</a>
+                <a href="omoss.html">About Us &#9916;</a>
+              </div>
+</nav>
