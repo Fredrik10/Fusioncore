54da5944fbf3717df1323754e2be1a61e78f5cc8
elev <elev@5CG62366CS>
2018-11-14 14:49:54 +0100

    updaterad

---
+++ fredrik/artiklar.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Fusion Core</title>
+<link rel="stylesheet" href="navigationbords.css">
+<script type="text/javascript" src="buttons.js"></script>
+</head>
+<body>
+<?php
+require"../templates/menu.php";
+require"../templates/masthead.php";
+?>
+  <h1>Patch Notes</h1>
+</body>
+</html>
---
+++ fredrik/fusioncore.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Fusion Core</title>
+<link rel="stylesheet" href="navigationbords.css">
+<script type="text/javascript" src="buttons.js"></script>
+</head>
+<body>
+ <?php
+require"../templates/menu.php";
+require"../templates/masthead.php";
+?>
+  
+ 
+  <img class="logo" src="fusioncore.png" alt="fusioncore logo">
+
+
+
+  <ul class = "buttonav button1">
+    <li><a class = "link1" href = "highscore.html">HighScore</a></li>
+    </ul>
+
+    <ul class= "buttonav button2">
+        <li><a class = "link1" href = "classes.html">Classes</a></li>
+    </ul>
+
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eligendi nesciunt fuga! Ipsa cum amet quam. Quaerat mollitia minus sit reiciendis cumque earum accusantium, nam soluta, rem possimus omnis reprehenderit.</p>
+    
+</body>
+</html>
